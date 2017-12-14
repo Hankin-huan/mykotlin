d905b1ddb2b154795238b2211f5863a37948ca6f
huangjin <hankin.huan@gmail.com>
2017-12-14 19:07:21 +0800

    exec

--- src/main/kotlin/hankin/mykotlin/classes/ClassesJava.java
+++ src/main/kotlin/hankin/mykotlin/classes/ClassesJava.java
@@ -2,6 +2,10 @@ package hankin.mykotlin.classes;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +39,23 @@ public class ClassesJava extends Y { // Y 是数据类
         System.out.println(b);
         System.out.println(f);
         System.out.println(g);
+
+        System.out.println("---------------------------------------------------------------------------------------");
+        try {
+            String arr[] = {"cmd.exe", "/c", "curl http://www.baidu.com"};//如果要https需要配置cacert.pem CA证书
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec(arr);//在cmd运行命令
+
+            InputStream inputStream = process.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream,"gb2312"));
+            String line = null;
+            while((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception eee) {
+            eee.printStackTrace();
+        }
+        System.out.println("---------------------------------------------------------------------------------------");
     }
 
 }
