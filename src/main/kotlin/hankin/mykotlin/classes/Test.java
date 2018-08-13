7f6ce2fe5af80eec99c550cbb7d07cf98e2f83fd
hankin <hankin.huan@gmail.com>
2018-08-13 22:06:28 +0800

    test

--- src/main/kotlin/hankin/mykotlin/classes/Test.java
+++ src/main/kotlin/hankin/mykotlin/classes/Test.java
@@ -13,18 +13,26 @@ public class Test {
 //        }
 
         // orl faces database 所有人脸数据图像
-        String orl = "E:\\opencv\\images\\orl_faces";
-        File file = new File(orl);
+//        String orl = "E:\\opencv\\images\\orl_faces";
+//        File file = new File(orl);
+//        String[] names = file.list();
+//        for (int i = 0; i < names.length; i++) {
+//            String dir = names[i];
+//            int num = Integer.valueOf(dir.substring(1));
+//            String child = orl+"\\"+dir;
+//            File f = new File(child);
+//            String imgs[] = f.list();
+//            for (int j = 0; j < imgs.length; j++) {
+//                System.out.println(child+"\\"+imgs[j]+";"+num);
+//            }
+//        }
+
+        // myfaces 所有人脸数据图像
+        String myfaces = "E:\\opencv\\images\\myfaces";
+        File file = new File(myfaces);
         String[] names = file.list();
         for (int i = 0; i < names.length; i++) {
-            String dir = names[i];
-            int num = Integer.valueOf(dir.substring(1));
-            String child = orl+"\\"+dir;
-            File f = new File(child);
-            String imgs[] = f.list();
-            for (int j = 0; j < imgs.length; j++) {
-                System.out.println(child+"\\"+imgs[j]+";"+num);
-            }
+            System.out.println(myfaces+"\\"+names[i]+";"+2);
         }
     }
 
